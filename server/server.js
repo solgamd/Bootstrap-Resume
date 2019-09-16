9b431b6868cc11c9cf4ac9bacfec28ade95e70aa
solgamd <solgamd@gmail.com>
2019-09-16 12:12:40 -0500

    Removed catch-all in server.js - TEST

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./public/index.html"))
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
