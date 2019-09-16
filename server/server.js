53d472edcab1834b0bf8b8d4c7058497e756e144
solgamd <solgamd@gmail.com>
2019-09-16 11:32:28 -0500

    Added catch-all in server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
